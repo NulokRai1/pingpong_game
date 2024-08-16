fced87a0ebacdba498d8c9ba4e13b906d5cfb8e0
NulokRai1 <nulokrai29@gmail.com>
2024-08-16 08:59:53 +0545

    Ping pong game

---
+++ main.py
@@ -0,0 +1,51 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(height=600, width=800)
+screen.bgcolor("black")
+screen.title("Pong Game")
+game_on = True
+screen.tracer(0)
+speed = 1
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(fun=r_paddle.go_up, key="Up")
+screen.onkey(fun=r_paddle.go_down, key="Down")
+screen.onkey(fun=l_paddle.go_up, key="w")
+screen.onkey(fun=l_paddle.go_down, key="s")
+
+while game_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # detect collision with paddle
+    if ball.xcor() > 320 and ball.distance(r_paddle) < 50 or ball.xcor() < -320 and ball.distance(l_paddle) < 50:
+        ball.bounce_x()
+
+    # detect if ball out of bound right
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # detect for left
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+    speed += 2
+    ball.speed(speed)
+
+screen.exitonclick()
